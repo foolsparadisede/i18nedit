585e905839c0cb45c34d37d9a3176085e78f18af
Maximilian Etti <maximilian_etti@yahoo.de>
2019-06-01 20:33:48 +0200

    SPA redirect

--- backend/src/main/kotlin/de/foolsparadise/i18lnedit/App.kt
+++ backend/src/main/kotlin/de/foolsparadise/i18lnedit/App.kt
@@ -7,8 +7,13 @@ import de.foolsparadise.i18lnedit.service.TranslationIOService
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CORS
+import io.ktor.http.content.file
+import io.ktor.http.content.static
+import io.ktor.http.content.staticRootFolder
+import io.ktor.response.respondRedirect
 import io.ktor.routing.get
 import io.ktor.routing.post
+import io.ktor.routing.route
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -71,11 +76,24 @@ fun main() {
         }
 
         routing {
-            get("/config") { controller.config(call) }
-            get("/items") { controller.items(call) }
-            get("/languages") { controller.languages(call) }
-            post("/reimport") { controller.reimport(call) }
-            post("/update") { controller.update(call) }
+            if (System.getenv("APP_HOME") != null) {
+                static("/") {
+                    staticRootFolder = File(System.getenv("APP_HOME") + "/www")
+                    file("index.html")
+                }
+            }
+
+            route("api") {
+                get("/config") { controller.config(call) }
+                get("/items") { controller.items(call) }
+                get("/languages") { controller.languages(call) }
+                post("/reimport") { controller.reimport(call) }
+                post("/update") { controller.update(call) }
+            }
+
+            get("/{...}") {
+                call.respondRedirect("/index.html")
+            }
         }
     }
     server.start(wait = true)
